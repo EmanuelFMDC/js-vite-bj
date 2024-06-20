eca15fe1ecad749391986f670d63c682f683df3d
Emanuel FMDC <emanuel2303e@gmail.com>
2024-06-20 14:12:25 -0600

    first commit

---
+++ src/usecases/crear-deck.js
@@ -0,0 +1,33 @@
+import _ from 'underscore'
+
+/**
+ * 
+ * @param {Array<String>} tipos Ejemplo: ['C','D','H','S']
+ * @param {Array<String>} especiales Ejemplo: ['A','J','Q','K']
+ * @returns {Array<String>} retorna un nuevo deck de cartas
+ */
+export const crearDeck = (tipos, especiales) => {
+
+    if(!tipos || tipos.length === 0)
+    throw new Error('Tipos de cartas es obligatorio como un arreglo de strings')
+
+    if(!especiales || especiales.length === 0)
+    throw new Error('Tipos de cartas es obligatorio como un arreglo de strings')
+    let deck = [];
+
+    for( let i = 2; i <= 10; i++ ) {
+        for( let tipo of tipos ) {
+            deck.push( i + tipo);
+        }
+    }
+
+    for( let tipo of tipos ) {
+        for( let esp of especiales ) {
+            deck.push( esp + tipo);
+        }
+    }
+    // console.log( deck );
+    deck = _.shuffle( deck );
+    console.log( deck );
+    return deck;
+}
---
+++ src/usecases/index.js
@@ -0,0 +1,5 @@
+export { pedirCarta } from './pedir-carta';
+export { valorCarta } from './valor-carta';
+export {crearDeck as crearNuevoDeck} from './crear-deck'
+export {turnoComputadora} from './turno-computadora'
+export{ crearCartaHTML } from './crear-carta'
